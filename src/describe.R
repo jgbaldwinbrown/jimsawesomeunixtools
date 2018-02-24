cbf0fd5f0d956e58daf0cad071e75fb964923761
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2018-02-23 23:08:47 -0700

    added sum to describe

--- src/describe.R
+++ src/describe.R
@@ -1,7 +1,6 @@
 #!/usr/bin/Rscript
 
 data=as.numeric(readLines("stdin"))
-cat("Number: ")
-cat(length(data))
-cat("\n")
+cat("Number: "); cat(length(data)); cat("\n")
+cat("Sum: "); cat(sum(data)); cat ("\n")
 summary(data)
