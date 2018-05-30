297b3f023d0436ceed9a23c520c5ae5e2f339e78
jgbaldwinbrown <jgbaldwinbrown@gmail.com>
2018-05-30 10:24:23 -0600

    added comments to tail.c

--- src/tail.c
+++ src/tail.c
@@ -13,12 +13,15 @@ int main(int argc, char *argv[]){
     int tailnum = atoi(argv[1]);
     char *lines[tailnum];
     int queueindex = 0;
+    /* Allocate starting memory for all tail line holders: */
     for (int i=0; i<tailnum; i++)
     {
         lines[i] = (char *)malloc(strlen("")+1);
         strcpy(lines[i],"");
     }
+    /* Allocate memory for line reading buffer */
     line = (char *)malloc(bufsize * sizeof(char));
+    /* for each line of input, add line to queue, and kick out oldest queue member */
     while ((nread = getline(&line, &bufsize, stdin)) != -1)
     {
         if (strlen(lines[queueindex]) < strlen(line))
@@ -28,12 +31,15 @@ int main(int argc, char *argv[]){
         strcpy(lines[queueindex],line);
         queueindex = new_index(queueindex,tailnum);
     }
+    /* We are done with the input buffer now, so free it: */
     free(line);
+    /* Print all strings in queue in order from oldest to newest: */
     for (int i=0; i<tailnum; i++)
     {
         printf("%s",lines[queueindex]);
         queueindex = new_index(queueindex,tailnum);
     }
+    /* Free all tail line holders: */
     for (int i=0; i<tailnum; i++)
     {
         free(lines[i]);
