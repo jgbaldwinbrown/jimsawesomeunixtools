8de0483c22f09abe3e5b8d3b7e84ce21dd105196
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2019-11-24 12:59:40 -0700

    column updated, still not working

--- src/column.c
+++ src/column.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define INIT_SIZE 5
 
-struct column {
+
+typedef struct column {
     char *content;
     size_t width;
     size_t cap;
-}
+} column;
 
-struct row {
+typedef struct row {
     column *col;
     size_t ncol;
     size_t col_cap;
 } row;
 
-struct table {
+typedef struct table {
     size_t nrows;
     size_t row_cap;
     size_t ncols;
@@ -23,18 +25,34 @@ struct table {
     row *rows;
 } table;
 
+void column_all() ;
+void fill_row(row *arow_ptr, char *line) ;
+void fill_table(FILE *fptr, table *atable_ptr);
+void print_row(row arow, size_t ncols, size_t *max_col_width) ;
+void print_table(table atable) ;
+void free_col(column acol) ;
+void free_row(row arow) ;
+void free_table(table atable) ;
+column init_column() ;
+column *init_columns() ;
+row init_row() ;
+row *init_rows() ;
+table init_table();
+
+
 table init_table(){
     table out;
     out.nrows = 0;
     out.row_cap = INIT_SIZE;
     out.ncols = 0;
     out.col_cap = INIT_SIZE;
-    out.max_col_width = malloc(sizeof(size_t) * INIT_SIZE)
+    out.max_col_width = malloc(sizeof(size_t) * INIT_SIZE);
     if (out.max_col_width == NULL) {
         fprintf(stdout, "out of memory\n");
         exit(1);
     }
-    rows = init_rows();
+    out.rows = init_rows();
+    return(out);
 }
 
 row *init_rows() {
@@ -52,7 +70,7 @@ row *init_rows() {
 row init_row() {
     row out;
     out.ncol = 0;
-    out.cap = INIT_SIZE;
+    out.col_cap = INIT_SIZE;
     out.col = init_columns();
     return(out);
 }
@@ -73,7 +91,7 @@ column init_column() {
     column out;
     out.width = 0;
     out.cap = INIT_SIZE;
-    out.content = malloc(INIT_SIZE * sizeof(char))
+    out.content = malloc(INIT_SIZE * sizeof(char));
     if (out.content == NULL) {
         fprintf(stdout, "out of memory\n");
         exit(1);
@@ -85,7 +103,7 @@ column init_column() {
 }
 
 void free_table(table atable) {
-    free(atable.max_col_widths)
+    free(atable.max_col_width);
     for (size_t i=0; i<atable.row_cap; i++){
         free_row(atable.rows[i]);
     }
@@ -93,7 +111,7 @@ void free_table(table atable) {
 }
 
 void free_row(row arow) {
-    for (i=0; i<arow.col_cap; i++) {
+    for (size_t i=0; i<arow.col_cap; i++) {
         free_col(arow.col[i]);
     }
     free(arow.col);
@@ -103,7 +121,82 @@ void free_col(column acol) {
     free(acol.content);
 }
 
-void column() {
+void print_table(table atable) {
+    printf("nrows: %lu\n", atable.nrows);
+    for (size_t i=0; i<atable.nrows; i++) {
+        print_row(atable.rows[i], atable.ncols, atable.max_col_width);
+    }
+}
+
+void print_row(row arow, size_t ncols, size_t *max_col_width) {
+    for(size_t i=0; (i < ncols) && (i < arow.ncol); i++) {
+        printf("%*s", max_col_width[i]+1, arow.col[i].content);
+    }
+    printf("\n");
+}
+
+void fill_table(FILE *fptr, table *atable_ptr){
+    char *line = NULL;
+    int rownum = 0;
+    size_t size = 0;
+    size_t len_read = 0;
+    
+    while ((len_read = getline(&line, &size, fptr)) != -1) {
+        if (rownum >= atable_ptr->row_cap) {
+            atable_ptr->rows = realloc(atable_ptr->rows, sizeof(row) * (2 * atable_ptr->row_cap));
+            if (atable_ptr->rows == NULL) {
+                fprintf(stderr, "out of memory!");
+                exit(1);
+            }
+            for (size_t i=atable_ptr->row_cap; i<(2 * atable_ptr->row_cap); i++){
+                atable_ptr->rows[i] = init_row();
+            }
+            atable_ptr->row_cap = 2 * atable_ptr->row_cap;
+        }
+        fill_row(&atable_ptr->rows[rownum], line);
+        rownum++;
+        atable_ptr->nrows = rownum;
+    } 
+    free(line);
+}
+
+void fill_row(row *arow_ptr, char *line) {
+        char *token = strtok(line, " 	");
+        size_t token_len = 0;
+        size_t col_num = 0;
+        while (token != NULL) {
+            token_len = strlen(token);
+            if (arow_ptr->col_cap <= col_num) {
+                arow_ptr->col = realloc(arow_ptr->col, sizeof(column) * (2 * arow_ptr->col_cap));
+                if (arow_ptr->col == NULL) {
+                    fprintf(stderr, "out of memory!");
+                    exit(1);
+                }
+                for (size_t i=arow_ptr->col_cap; i<(2 * arow_ptr->col_cap); i++){
+                    arow_ptr->col[i] = init_column();
+                }
+                arow_ptr->col_cap = 2 * arow_ptr->col_cap;
+            }
+            if (arow_ptr->col[col_num].cap < (token_len + 1)) {
+                arow_ptr->col[col_num].content = realloc(arow_ptr->col[col_num].content, (token_len+1) * sizeof(char));
+                if (arow_ptr->col[col_num].content == NULL) {
+                    fprintf(stderr, "out of memory!");;
+                    exit(1);
+                }
+                arow_ptr->col[col_num].cap = token_len+1;
+            }
+            strncpy(arow_ptr->col[col_num].content, token, arow_ptr->col[col_num].cap);
+            /*printf("content: %s\n", arow_ptr->col[col_num].content);*/
+            
+            arow_ptr->col[col_num].width = token_len;
+            
+            arow_ptr->ncol = col_num;
+            token = strtok(NULL, " 	");
+            col_num++;
+        }
+}
+
+void column_all() {
     FILE *fptr = stdin;
     table atable = init_table();
     
@@ -114,6 +207,38 @@ void column() {
 }
 
 int main(int argc, char *argv[]){
-    column();
+    column_all();
     return(0);
 }
+
+/*
+#include <stdio.h>
+
+int main() {
+    char *line = NULL;
+    int findex = 0;
+    size_t size;
+    
+    while (getline(&line, &size, stdin) != -1) {
+        findex++;
+        if (findex%4 == 1) {
+            printf(">%s", line+1);
+        }
+        if (findex%4 == 2) {
+            printf("%s",line);
+        }
+    } 
+    return(0);
+}
+*/
+
+/*
+        nc += strlen(line);
+        char *token = strtok(line, " ");
+        while (token != NULL)
+        {
+            nf++;
+            token = strtok(NULL, " ");
+        }
+        nr++;
+*/
